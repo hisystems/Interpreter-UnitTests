cb69cc69e451d78725a0e766279fd351fd8ee401
Toby Wicks <toby@hisystems.com.au>
2012-08-08 12:33:04 +0930

    Added tests for boolean constants.

---
+++ Tests/BooleanTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HiSystems.Interpreter.UnitTests
+{
+	[TestFixture]
+	public class BooleanTests
+	{
+        public static Engine Engine = new Engine();
+        
+		[Test]
+		public void TrueConstant()
+		{
+            Assert.That(Engine.Parse("true").Execute<Boolean>(), Is.EqualTo((Boolean)true));
+		}
+		
+        [Test]
+        public void FalseConstant()
+        {
+            Assert.That(Engine.Parse("false").Execute<Boolean>(), Is.EqualTo((Boolean)false));
+        }
+        
+	}
+}
